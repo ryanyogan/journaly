e8ab44876d1daeca326f6ab0f612f3d170e69227
Ryan Yogan <ryanyogan@gmail.com>
2024-04-25 16:35:26 -0500

    install tailwind and config, basic routes

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import "./styles.css";
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,48 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Journal" },
+    { name: "description", content: "Write it ... or it didn't happen" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="p-10">
+      <h1 className="text-5xl">Work Journal</h1>
+      <p className="mt-2 text-lg text-gray-400">
+        Learnings and doings. Updated weekly.
+      </p>
+
+      <div className="mt-6">
+        <p className="font-bold">
+          Week of April 20<sup>th</sup>
+        </p>
+
+        <div className="mt-3 space-y-4">
+          <div>
+            <p>Work</p>
+            <ul className="ml-8 list-disc">
+              <li>First Item</li>
+              <li>Second Item</li>
+            </ul>
+          </div>
+          <div>
+            <p>Learnings</p>
+            <ul className="ml-8 list-disc">
+              <li>First Item</li>
+              <li>Second Item</li>
+            </ul>
+          </div>
+          <div>
+            <p>Interesting things</p>
+            <ul className="ml-8 list-disc">
+              <li>First Item</li>
+              <li>Second Item</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
